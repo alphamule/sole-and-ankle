48918bd0b5de8ef937233f3872836aa472b8757d
Eric Wagner <github@alphamule.com>
2021-03-31 17:28:11 -0700

    Completed Exercise 2: Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,9 +29,17 @@ const Header = () => {
 const MainHeader = styled.div`
   padding: 0 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  height: 72px;
+  align-items: center;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  display: flex;
+  gap: 48px;
+  flex: 1;
+  justify-content: center;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
