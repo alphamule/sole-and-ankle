94c6eb7c85e2bb98625b5ce50d060ee04de53781
Eric Wagner <github@alphamule.com>
2021-03-31 17:37:01 -0700

    Completed Exercise 4a: Grid layout

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -53,15 +53,21 @@ const ShoeCard = ({
 const Link = styled.a`
   text-decoration: none;
   color: inherit;
+  flex: 1;
 `;
 
-const Wrapper = styled.article``;
+const Wrapper = styled.article`
+  max-width: 600px;
+  min-width: 300px;
+`;
 
 const ImageWrapper = styled.div`
   position: relative;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+  width: 100%;
+`;
 
 const Row = styled.div`
   font-size: 1rem;
